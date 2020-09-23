9a6417c62417e07621db9eb56dfda7a4699dfdaa
Rimas <rikelmegriep1@gmail.com>
2020-09-23 08:22:46 -0300

    web: start detailed spent page

--- web/src/pages/DetailedSpent/index.tsx
+++ web/src/pages/DetailedSpent/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { FiLoader } from 'react-icons/fi'
 
-const DetailedSpent: React.FC = () => {
+import './styles.css'
+
+interface Params {
+    id: string
+}
+
+const DetailedSpent: React.FC = (props) => {
+    const { id } = useParams<Params>()
+
     return (
-        <h1>Hello</h1>
+        <main>
+        </main>
     )
 }
 
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from '@unform/web'
 import { Input } from '../../components/Form'
 import { useAuth } from '../../contexts/auth'
 
 import './styles.css'
+import { FiLoader } from 'react-icons/fi'
 
 const Login: React.FC = () => {
     const { login } = useAuth()
+    const [loading, setLoading] = useState(false)
 
     return (
         <main id="loginPage">
             <h2>Faça login na sua conta!</h2>
-            <Form onSubmit={data => login(data).catch(err => alert('Falha ao logar-se, tente novamente'))}>
+            <Form onSubmit={data => {
+                setLoading(true)
+                login(data).catch(err => {
+                    setLoading(false)
+                    alert('Falha ao logar-se, tente novamente')
+                })
+            }}>
                 <Input name="username" autoComplete="off" placeholder="Username" className="input" required />
                 <Input name="password" placeholder="Senha" className="input" type="password" required />
-                <button className="btn" type="submit">Login</button>
+                <button className="btn" type="submit">Login {loading && 
+                    <FiLoader className="spin" />
+                }</button>
             </Form>
         </main>
     )
